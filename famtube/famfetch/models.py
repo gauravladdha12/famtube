bd6b7139f80d8d1c917d06ce1be7423d08d1622c
Gaurav Laddha <gladdha@amazon.com>
2022-11-25 22:51:29 +0530

    FAM: added video model

--- famtube/famfetch/models.py
+++ famtube/famfetch/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class videos(models.Model):
+    id = models.BigAutoField(
+        editable=False,
+        primary_key=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    title = models.TextField()
+
+    description = models.TextField()
+
+    publish_date_time = models.DateTimeField(null=False)
+
+    thumbnail_url = models.URLField(max_length=1000, blank=True, null=True)
